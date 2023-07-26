01b81beb36a8e158344ef7ac2b58e227640314a9
AlexAXC <alex@asicxchange.com>
2023-07-26 16:24:47 -0400

    changed redirect destination

--- assets/js/regform.js
+++ assets/js/regform.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const form = document.querySelector(".contact-form");
     form.action = url;
 
-    form.querySelector('input[name="_next"]').value = "http://127.0.0.1:5500/";
+    form.querySelector('input[name="_next"]').value = "index.html";
     form.querySelector('input[name="_subject"]').value = "New Lead!";
     form.querySelector('input[name="_template"]').value = "table";
     form.querySelector('input[name="_captcha"]').value = "false";
